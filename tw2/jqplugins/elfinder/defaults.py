fca3540727206652f20b276af67bf47c29a79669
Robert Sudwarts <robert.sudwarts@gmail.com>
2014-02-05 09:19:20 +0000

    modified:   tw2/jqplugins/elfinder/defaults.py         modified:   tw2/jqplugins/elfinder/widgets.py 	deleted:    wingproject.wpr

--- tw2/jqplugins/elfinder/defaults.py
+++ tw2/jqplugins/elfinder/defaults.py
@@ -2,5 +2,7 @@
 _plugin_css_dirname_ = 'static/jquery/plugins/%(name)s/%(version)s/%(subdir)s'
 _elfinder_name_       = 'elfinder'
 _elfinder_basename_       = 'elfinder'
-_elfinder_version_        = 'trunk'
-_elfinder_debug_          = 'full'
+_elfinder_version_        = '2.0-rc1'
+# no 'full' available in the 2.0-rc1 package downloaded
+#_elfinder_debug_          = 'full'
+_elfinder_debug_          = 'min' 
--- tw2/jqplugins/elfinder/widgets.py
+++ tw2/jqplugins/elfinder/widgets.py
@@ -10,21 +10,32 @@ from tw2.jqplugins.elfinder import defaults
 
 modname = 'tw2.jqplugins.elfinder'
 
-#elRTE
-elfinder_images = jqbase.DirLink(modname=modname, filename=defaults._plugin_css_dirname_ % dict(name=defaults._elfinder_name_, version=defaults._elfinder_version_, subdir="images"))
-elfinder_css = jqbase.jQueryPluginCSSLink(resources=[elfinder_images], name=defaults._elfinder_name_,
+dict_img = dict(name=defaults._elfinder_name_, version=defaults._elfinder_version_, subdir="img")
+elfinder_images = jqbase.DirLink(
+    modname=modname, 
+    filename=defaults._plugin_css_dirname_ % dict_img
+    )
+
+elfinder_css = jqbase.jQueryPluginCSSLink(
+    resources=[elfinder_images], name=defaults._elfinder_name_,
     basename='%s' % (defaults._elfinder_basename_),
     version=defaults._elfinder_version_,
     modname = modname
     )
-elfinder_js = jqbase.jQueryPluginJSLink(name=defaults._elfinder_name_,
+
+elfinder_js = jqbase.jQueryPluginJSLink(
+    name=defaults._elfinder_name_,
     basename='%s.%s' % (defaults._elfinder_basename_, defaults._elfinder_debug_),
     modname = modname,
-    version=defaults._elfinder_version_)
+    version=defaults._elfinder_version_
+    )
 
-elfinder = jqbase.jQueryJSLink(resources=[jquibase.jquery_ui_js, elfinder_css, elfinder_js])
+elfinder = jqbase.jQueryJSLink(
+    resources=[jquibase.jquery_ui_js, elfinder_css, elfinder_js]
+    )
 
 class elFinderWidget(jquibase.JQueryUIWidget):
+
     resources = [jquibase.jquery_ui_js, elfinder_css, elfinder_js]
     url = twc.params.Param('Connector URL', default=twc.params.Required)
     template = 'tw2.jqplugins.elfinder.templates.widget'
