bac2b0c2ea3595d278c50a6a8cb76bdca8118cf3
Robert Sudwarts <robert.sudwarts@gmail.com>
2014-02-05 12:24:17 +0000

    Moved v1 connector to a new directory -- with its original contents Added a new v2_connector directory to hold the elfinder.connector associated middleware etc etc
    	modified:   tw2/jqplugins/elfinder/base.py
    	new file:   tw2/jqplugins/elfinder/v1_connector/__init__.py
    	new file:   tw2/jqplugins/elfinder/v1_connector/cgi.py
    	new file:   tw2/jqplugins/elfinder/v1_connector/elFinder.py
    	new file:   tw2/jqplugins/elfinder/v2_connector/__init__.py
    	new file:   tw2/jqplugins/elfinder/v2_connector/connector.py
    	new file:   tw2/jqplugins/elfinder/v2_connector/exceptions.py
    	new file:   tw2/jqplugins/elfinder/v2_connector/middleware.py
    	new file:   tw2/jqplugins/elfinder/v2_connector/utils/__init__.py
    	new file:   tw2/jqplugins/elfinder/v2_connector/utils/volumes.py
    	modified:   tw2/jqplugins/elfinder/widgets.py
    	deleted:    tw2/jqplugins/elfinder/connector/__init__.py
    	deleted:    tw2/jqplugins/elfinder/connector/cgi.py
    	deleted:    tw2/jqplugins/elfinder/connector/elFinder.py

--- tw2/jqplugins/elfinder/base.py
+++ tw2/jqplugins/elfinder/base.py
@@ -3,9 +3,40 @@
 import tw2.core as twc
 from tw2.core.resources import encoder
 
-# tw2.jquery imports
+# tw2.jquery & tw2.japlugins.ui imports
+from tw2.jquery import base as jqbase
+from tw2.jqplugins.ui import base as jquibase
+
 # import from *this* package
 from tw2.jqplugins.elfinder import defaults
 
+modname = 'tw2.jqplugins.elfinder'
 ### Resources
+
+dict_img = dict(name=defaults._elfinder_name_,
+                version=defaults._elfinder_version_,
+                subdir="img"
+                )
+
+elfinder_images = jqbase.DirLink(
+    modname=modname,
+    filename=defaults._plugin_css_dirname_ % dict_img
+    )
+
+elfinder_css = jqbase.jQueryPluginCSSLink(
+    resources=[elfinder_images], name=defaults._elfinder_name_,
+    basename='%s.%s' % (defaults._elfinder_basename_, defaults._elfinder_debug_),
+    version=defaults._elfinder_version_,
+    modname = modname,
+    )
+
+elfinder_js = jqbase.jQueryPluginJSLink(
+    name=defaults._elfinder_name_,
+    basename='%s.%s' % (defaults._elfinder_basename_, defaults._elfinder_debug_),
+    version=defaults._elfinder_version_,
+    modname = modname,
+    )
+
+
+
 
--- tw2/jqplugins/elfinder/widgets.py
+++ tw2/jqplugins/elfinder/widgets.py
@@ -1,47 +1,25 @@
 
 # tw2-proper imports
 import tw2.core as twc
-from tw2.jquery import base as jqbase, defaults as jqdefaults
 
-# imports from this package
 from tw2.jqplugins.ui import base as jquibase
-from tw2.jqplugins.elfinder import base as elfinderbase
-from tw2.jqplugins.elfinder import defaults
+import base as elfbase
 
-modname = 'tw2.jqplugins.elfinder'
-
-dict_img = dict(name=defaults._elfinder_name_, version=defaults._elfinder_version_, subdir="img")
-elfinder_images = jqbase.DirLink(
-    modname=modname, 
-    filename=defaults._plugin_css_dirname_ % dict_img
-    )
-
-elfinder_css = jqbase.jQueryPluginCSSLink(
-    resources=[elfinder_images], name=defaults._elfinder_name_,
-    basename='%s' % (defaults._elfinder_basename_),
-    version=defaults._elfinder_version_,
-    modname = modname
-    )
-
-elfinder_js = jqbase.jQueryPluginJSLink(
-    name=defaults._elfinder_name_,
-    basename='%s.%s' % (defaults._elfinder_basename_, defaults._elfinder_debug_),
-    modname = modname,
-    version=defaults._elfinder_version_
-    )
-
-elfinder = jqbase.jQueryJSLink(
-    resources=[jquibase.jquery_ui_js, elfinder_css, elfinder_js]
-    )
 
 class elFinderWidget(jquibase.JQueryUIWidget):
 
-    resources = [jquibase.jquery_ui_js, elfinder_css, elfinder_js]
+    resources = [jquibase.jquery_ui_js,
+                 elfbase.elfinder_css,
+                 elfbase.elfinder_js
+                 ]
+
     url = twc.params.Param('Connector URL', default=twc.params.Required)
     template = 'tw2.jqplugins.elfinder.templates.widget'
 
     def prepare(self):
+
         if 'url' not in self.options:
             assert hasattr(self, 'url'), 'No url provided.   Please supply the url parameter'
             self.options['url'] = self.url
+
         super(elFinderWidget, self).prepare()
